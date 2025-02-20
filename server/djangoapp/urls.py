bce9f0a8c2fd53260c6500a4c70fedd9ba79e32f
Qatam <qatratqatam@gmail.com>
2025-02-20 13:49:51 +0300

    Creating CarModel & CarMake\

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,4 +1,3 @@
-# Uncomment the imports before you add the code
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
@@ -18,5 +17,8 @@ urlpatterns = [
     # path for dealer reviews view
 
     # path for add a review view
+
+    # path for get_cars view
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
