9fcfce53619232ecaf4f1d7a892f34a56ef9421f
Qatam <96803843+AnasAlhwid@users.noreply.github.com>
2025-02-24 13:21:26 +0300

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,4 +1,5 @@
-port os
+import requests
+import os
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -12,11 +13,9 @@ sentiment_analyzer_url = os.getenv(
 def get_request(endpoint, **kwargs):
     params = ""
     if(kwargs):
-        for key,value in kwargs.items():
-            params=params+key+"="+value+"&"
-
+        for key, value in kwargs.items():
+            params = params+key+"="+value+"&"
     request_url = backend_url+endpoint+"?"+params
-
     print("GET from {} ".format(request_url))
     try:
         # Call get method of requests library with URL and parameters
